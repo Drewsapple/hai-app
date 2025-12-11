ceb1634d7171f719bafc7689a1084c90a58af336
Drew Fisher <drew@pods.media>
2025-12-11 12:39:19 -0500

    add eslint-plugin-unused-imports for lint autofix of unused imports vs other unused vars

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,46 +1,49 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'import', 'jsx-a11y'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:import/recommended',
-    'plugin:jsx-a11y/recommended',
-    'prettier',
-  ],
-  settings: {
-    react: { version: 'detect' },
-    'import/resolver': {
-      typescript: {
-        project: './tsconfig.json',
-      },
-      alias: {
-        map: [['~', './src']],
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-      },
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'import', 'jsx-a11y', 'unused-imports'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:import/recommended',
+        'plugin:jsx-a11y/recommended',
+        'prettier',
+    ],
+    settings: {
+        react: { version: 'detect' },
+        'import/resolver': {
+            typescript: {
+                project: './tsconfig.json',
+            },
+            alias: {
+                map: [['~', './src']],
+                extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+            },
+        },
     },
-  },
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'react/display-name': 'off',
-  },
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      rules: {
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/ban-types': 'off',
-        'import/named': 'off',
-      },
+    rules: {
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'react/display-name': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+            'warn',
+            { vars: 'all', argsIgnorePattern: '^_', args: 'after-used', varsIgnorePattern: '^_' },
+        ],
     },
-  ],
+    overrides: [
+        {
+            files: ['**/*.ts', '**/*.tsx'],
+            rules: {
+                '@typescript-eslint/no-explicit-any': 'off',
+                '@typescript-eslint/ban-types': 'off',
+                'import/named': 'off',
+            },
+        },
+    ],
 }
-
-
